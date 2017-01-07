8ddd02b36cad6aa1bd7692d2e42ca3e06c2b633e
Logan Brick <Logan Brick>
2017-01-06 18:16:44 -0600

    Adding the basics of movement and game reset when reaching the water.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(startX, startY, bugSpeed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,8 +8,9 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
 
     //set default start location of enemy
-    this.x = 0;
-    this.y = 300;
+    this.x = startX;
+    this.y = startY;
+    this.speed = bugSpeed;
 };
 
 // Update the enemy's position, required method for game
@@ -18,6 +19,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+     if ( this.x < 606) {
+        this.x += this.speed * dt;
+    } else {
+      //when bug hits end of screen, start at common point
+        this.x = -101;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,16 +43,37 @@ var Player = function() {
     this.sprite = 'images/char-boy.png';
 
     //default starting location of the player
-    this.x = 0;
-    this.y = 0;
+    this.x = 202;
+    this.y = 404;
 };
 
-// Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+// Handle the input to move the player around
+Player.prototype.handleInput = function(direction) {
+    if (direction === 'right') {
+      this.x += 101;
+    } else if (direction === 'left') {
+      this.x -= 101;
+    } else if (direction === 'down') {
+      this.y += 85;
+    } else if (direction === 'up') {
+      this.y -= 85;
+      //reset the player position when the water is reached
+      if (this.y <= 50) {
+        this.reset();
+      }
+    }
+
+};
+
+//Reset the position of the player
+Player.prototype.reset = function() {
+    this.x = 202;
+    this.y = 404;
+}
+
+// This will be the collision detection function
+Player.prototype.update = function() {
+    // This will be the
 };
 
 // Draw the player on the screen, required method for game
@@ -54,11 +82,21 @@ Player.prototype.render = function() {
 };
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var enemy1 = new Enemy();
+// 3 Enemies with different locations
+var enemy1 = new Enemy(-101, 55, 60);
+var enemy2 = new Enemy(-303, 55, 86);
+var enemy3 = new Enemy(-101, 140, 120);
+var enemy4 = new Enemy(-101, 225, 60);
+var enemy5 = new Enemy(-202, 225, 100);
 
+// Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 allEnemies.push(enemy1);
+allEnemies.push(enemy2);
+allEnemies.push(enemy3);
+allEnemies.push(enemy4);
+allEnemies.push(enemy5);
+
 
 // Place the player object in a variable called player
 var player = new Player();
